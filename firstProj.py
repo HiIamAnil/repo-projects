861475d58d1397c4803c1afc9d952cf352bf066c
HiIamAnil <forotheruse4mi@gmail.com>
2019-03-19 22:22:31 +0530

    update madta idini

--- firstProj.py
+++ firstProj.py
@@ -2,4 +2,4 @@ alphabet    ='abcdefghijklmnopqrstuvwxyz'
 key         =4
 message     =input('enter the input message')
 
-# i wanna say this is indicated after the merge
+# i wannagit  say this is indicated after the merge
